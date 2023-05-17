efb28266d8e901a088df6acc7974763417ee86ba
Kanjanapas <oshin_2544@hotmail.com>
2023-05-18 00:05:05 +0700

    tsc app.ts section 18: 79 - 84

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('Todolist');
+const btnEl = document.getElementById('btn');
+const inputEl = document.getElementById('input');
+const formEl = document.querySelector('form');
+const listEl = document.getElementById('list');
+formEl.addEventListener('submit', saveData);
+function saveData(e) {
+    e.preventDefault();
+    const message = inputEl.value;
+    createList(message);
+}
+function createList(text) {
+    const liEl = document.createElement('li');
+    liEl.append(text);
+    listEl.append(liEl);
+    inputEl.value = '';
+}
