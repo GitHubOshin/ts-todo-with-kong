ecbde5948f3c18dbd469026017c8f79c570d71cd
OSHIN <izkp44@gmail.com>
2023-05-17 23:58:36 +0700

    List

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 const btnEl = document.getElementById('btn')! as HTMLButtonElement
 const inputEl = document.getElementById('input')! as HTMLInputElement
 const formEl = document.querySelector('form')!
+const listEl = document.getElementById('list')!
 
 formEl.addEventListener('submit', saveData)
 
 function saveData(e: SubmitEvent) {
   e.preventDefault()
-  console.log(inputEl.value)
+  const message = inputEl.value
+  createList(message)
 }
 
-// btnEl.addEventListener('click', function () {
-//   console.log(inputEl.value)
-// })
+function createList(text: string) {
+  const liEl = document.createElement('li')
+  liEl.append(text)
+  listEl.append(liEl)
+  inputEl.value = ''
+}
